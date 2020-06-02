32a86a7a98f10345f799ed609e9c946486ff11c6
linus-bit <linus.buddrus@web.de>
2020-06-02 19:45:44 +0200

    update Funktion gesäubert

--- Rust_Waldbrand-mitgrafik/firesim/src/main.rs
+++ Rust_Waldbrand-mitgrafik/firesim/src/main.rs
@@ -7,7 +7,6 @@ use ggez::timer;
 use ggez::{Context, ContextBuilder, GameResult};
 use rand::{Rand, Rng};
 
-
 const SCREEN_WIDTH: f32 = 1000.0;
 const SCREEN_HEIGHT: f32 = 1000.0;
 const ENTRY_WIDTH: f32 = SCREEN_WIDTH / 100.0;
@@ -17,8 +16,8 @@ const BURNT_AGE: u32 = 650;
 
 #[derive(Debug, Copy, Clone)]
 enum Entry {
+    Edge,
     Empty,
-	Empty_brand,
     Tree,
     Fire(u32),
     Burnt(u32),
@@ -27,9 +26,8 @@ enum Entry {
 impl Rand for Entry {
     fn rand<R: Rng>(rng: &mut R) -> Self {
 	match rng.gen::<f32>() {
-	    i if i < 0.1 => Entry::Empty_brand,
-	    _ =>  Entry::Tree,
-	    
+	    i if i < 0.1 => Entry::Empty,
+	    _ => Entry::Tree,
 	}
     }
 }
@@ -39,13 +37,12 @@ struct State {
     fire_prob: f32,
     spawn_tree_prob: f32,
     neighbours: [(i8, i8); 8],
-    finished: bool,
 }
 
 impl State {
     fn new(ctx: &mut Context) -> State {
 	let mut rng = rand::thread_rng();
-	let mut grid = [[Entry::Empty; 100]; 100];
+	let mut grid = [[Entry::Edge; 100]; 100];
 	for y in 1..99 {
 	    for x in 1..99 {
 		grid[y][x] = rng.gen();
@@ -53,7 +50,7 @@ impl State {
 	}
 	let rand_x = rng.gen_range(1, 99);
 	let rand_y = rng.gen_range(1, 99);
-	grid [rand_x] [rand_y] = Entry::Fire(FIRE_AGE);
+	grid[rand_x][rand_y] = Entry::Fire(FIRE_AGE);
 	State {
 	    grid,
 	    fire_prob: 0.002,
@@ -68,30 +65,22 @@ impl State {
 		(1, 0),
 		(1, 1),
 	    ],
-	    finished: false,
 	}
     }
 }
 
 impl event::EventHandler for State {
     fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
-	
 	if timer::check_update_time(ctx, 60) {
 	    for y in 1..99 {
 		for x in 1..99 {
-		    if let Entry::Empty_brand = self.grid[x][y] {
-				if rand::thread_rng().gen::<f32>() < self.spawn_tree_prob {
-					self.grid[x][y] = Entry::Tree
-				}
-		    }
 		    self.grid[x][y] = match self.grid[x][y] {
 			Entry::Tree => {
 			    let mut fire_p = false;
 			    for (dx, dy) in self.neighbours.iter() {
 				if let Entry::Fire(_) =
 				    self.grid[(x as i8 + dx) as usize][(y as i8 + dy) as usize]
-				{	
-					
+				{
 				    if rand::thread_rng().gen::<f32>() < self.fire_prob {
 					fire_p = true;
 					break;
@@ -112,14 +101,21 @@ impl event::EventHandler for State {
 				Entry::Burnt(BURNT_AGE)
 			    }
 			}
+			Entry::Empty => {
+			    if rand::thread_rng().gen::<f32>() < self.spawn_tree_prob {
+				Entry::Tree
+			    } else {
+				Entry::Empty
+			    }
+			}
 			Entry::Burnt(age) => {
 			    if age >= 1 {
 				Entry::Burnt(age - 1)
 			    } else {
-				Entry::Empty_brand
+				Entry::Empty
 			    }
 			}
-			_ => Entry::Empty_brand,
+			_ => Entry::Empty,
 		    }
 		}
 	    }
@@ -143,13 +139,11 @@ impl event::EventHandler for State {
 	}
 	const RED: Color = Color::new(1.0, 0.0, 0.0, 1.0);
 	const GREEN: Color = Color::new(0.0, 1.0, 0.0, 1.0);
-	const BROWN: Color = Color::new(0.46,0.16,0.16,1.0);
-	const GREY: Color = Color::new(0.5,0.5,0.5,1.0);
+	const BROWN: Color = Color::new(0.46, 0.16, 0.16, 1.0);
+	const GREY: Color = Color::new(0.5, 0.5, 0.5, 1.0);
 	graphics::clear(ctx, graphics::BLACK);
 	let dst = nalgebra::Point2::new(0.0, 0.0);
 	let mb = &mut MeshBuilder::new();
-	// let (s_width, s_height) = graphics::size(ctx);
-	// println!("{} {}", s_width, s_height);
 	let mut counter: (f32, f32) = (0.0, 0.0);
 	for y in 0..100 {
 	    for x in 0..100 {
@@ -163,11 +157,11 @@ impl event::EventHandler for State {
 		    Entry::Tree => {
 			assign_rect(mb, GREEN, &mut counter);
 		    }
-			Entry::Empty_brand => {
-			assign_rect(mb, BROWN, &mut counter);
-			}
 		    Entry::Empty => {
-			assign_rect(mb,graphics::WHITE, &mut counter);
+			assign_rect(mb, BROWN, &mut counter);
+		    }
+		    Entry::Edge => {
+			assign_rect(mb, graphics::WHITE, &mut counter);
 		    }
 		}
 	    }
